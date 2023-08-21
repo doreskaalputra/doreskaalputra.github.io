9d5dfdf1aa1a340ea64a31de94e4a7e1c7ba78af
doreskaalputra <91145925+doreskaalputra@users.noreply.github.com>
2023-08-21 14:25:10 +0700

    Add files via upload
    edit cosde

--- js/script.js
+++ js/script.js
@@ -5,11 +5,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar side bar untuk mengehilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toogle class active untuk search form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('search-button');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
